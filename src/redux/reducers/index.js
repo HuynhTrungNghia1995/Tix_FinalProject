4224543d44faa573f78ed994149a413c9b6ce044
vuvoivang <vuprotk21@gmail.com>
2021-05-04 22:52:09 +0700

    push cho Duy

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from "../../containers/AdminTemplate/Login/modules/reducer"
 import { fetchFilmListReducer } from "../../containers/AdminTemplate/FilmManagement/modules/reducer"
+import systemCinemaReducer from "../../components/Schedule/modules/reducer";
 const store = combineReducers({
     authReducer,
     fetchFilmListReducer,
+    systemCinemaReducer,
 });
 
 export default store;
