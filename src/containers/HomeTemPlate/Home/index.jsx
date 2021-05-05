b39e08be278dca99243be244bf6c1aa524999a3d
DuyOnix <dtduy2210@gmail.com>
2021-05-05 09:06:09 +0700

    onix update home

--- src/containers/HomeTemPlate/Home/index.jsx
+++ src/containers/HomeTemPlate/Home/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Application from "../../../components/Application";
+import Carousel from "../../../components/Carousel";
 import Footer from "../../../components/Footer";
 import NavbarHome from "../../../components/NavbarHome";
+import "./style.css";
 
 export default function Home(props) {
+  const handleBackToTop = () => {
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  };
   return (
-    <div>
+    <div className="home">
       <NavbarHome />
+      <Carousel />
       <Application />
       <Footer />
+      <span onClick={handleBackToTop} id="backtotop" className="backtotop">
+        <i class="fa fa-angle-up"></i>
+      </span>
     </div>
   );
 }
