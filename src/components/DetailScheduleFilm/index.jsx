6991be1bbb15ca508665465f86c824ca15dee77d
vuvoivang <vuprotk21@gmail.com>
2021-05-30 13:04:49 +0700

    lich chieu

--- src/components/DetailScheduleFilm/index.jsx
+++ src/components/DetailScheduleFilm/index.jsx
@@ -1,413 +1,157 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-export default function DetailScheduleFilm() {
+export default function DetailScheduleFilm(props) {
+  const showTimesFilm = props.showTimesFilm;
+  console.log(showTimesFilm);
+  const [notExistFilmOfDay, setnotExistFilmOfDay] = useState(false);
+  const clickButtonDay = (index) => {
+    console.log(notExistFilmOfDay);
+    return () => {
+      if (index % 3 == 1) {
+        setnotExistFilmOfDay(true);
+      } else setnotExistFilmOfDay(false);
+    };
+  };
+  const renderDay = (i, date, dayOfWeek) => {
+    return (
+      <li className="detail__listOfDay--item nav-item">
+        <a
+          className={i == 0 ? "nav-link active" : "nav-link"}
+          onClick={clickButtonDay(i)}
+          data-toggle="tab"
+          role="tab"
+          aria-selected="true"
+        >
+          <p className="dayOfWeek">
+            {dayOfWeek != 0 ? `Thứ ${dayOfWeek + 1}` : "Chủ Nhật"}
+          </p>
+          <p className="date">{date}</p>
+        </a>
+      </li>
+    );
+  };
+  const renderListOfDays = () => {
+    const startDay = new Date(Date.now());
+    let i = 0;
+    let result = [];
+    while (i < 14) {
+      result.push(renderDay(i, startDay.getDate(), startDay.getDay()));
+      startDay.setDate(startDay.getDate() + 1);
+      i++;
+    }
+    return result;
+  };
+  const renderHeThongRap = () => {
+    return showTimesFilm?.heThongRapChieu.map((cumRap, index) => {
+      return (
+        <div
+          className={index == 0 ? "logo__wrapper active" : "logo__wrapper"}
+          data-toggle="tab"
+          role="tab"
+          data-target={"#" + cumRap.maHeThongRap}
+          aria-selected="true"
+        >
+          <div className="logo__detail">
+            <img
+              className="theaterList__image"
+              src={cumRap.logo}
+              alt={cumRap.tenHeThongRap}
+            />
+            <span className="tenHeThong">{cumRap.tenHeThongRap}</span>
+            <span className="arrow" />
+          </div>
+        </div>
+      );
+    });
+  };
+  const xuLyLichChieu = (cumRap) => {
+    if (cumRap.cumRapChieu.length == 0)
+      return (
+        <div className="alert alert-info">
+          Hiện không có lịch chiếu trên hệ thống rạp này
+        </div>
+      );
+    else if (notExistFilmOfDay)
+      return (
+        <div className="alert alert-danger">
+          Hiện ngày này không có lịch chiếu
+        </div>
+      );
+
+    return cumRap.cumRapChieu.map((rap, index) => {
+      const subName = rap.tenCumRap.split("-");
+      return (
+        <div className="wrapper__collapse ">
+          <div
+            className="main__collapse"
+            data-toggle="collapse"
+            data-target={"#" + cumRap.maHeThongRap + rap.maCumRap}
+          >
+            <div className="cinema__details--item">
+              <img
+                className="theaterList__image"
+                src="/images/bhd-star-bitexco-15379552241200.jpg"
+                alt="cgv"
+              />
+              <div className="wrapInfo">
+                <span className="chiNhanh">
+                  <span className="tenRap CGV">{subName[0]}</span> -{" "}
+                  {subName[1]}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="collapse" id={cumRap.maHeThongRap + rap.maCumRap}>
+            <div className="pt-3 row content__collapse stack">
+              <div className="col-12" style={{ padding: "0 15px" }}>
+                2D Digital
+              </div>
+              <div className="col-12">
+                <button className="btn btn-time">
+                  <span className="start">8:10</span> ~ 10:20
+                </button>
+                <button className="btn btn-time">
+                  <span className="start">13:10</span> ~ 15:20
+                </button>
+
+                <button className="btn btn-time">
+                  <span className="start">16:10</span> ~ 20:30
+                </button>
+                <button className="btn btn-time">
+                  <span className="start">20:10</span> ~ 22:10
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  const renderLichChieuChiNhanh = () => {
+    return showTimesFilm?.heThongRapChieu.map((cumRap, index) => {
+      return (
+        <div
+          className={index == 0 ? "tab-pane fade active show" : "tab-pane fade"}
+          id={cumRap.maHeThongRap}
+        >
+          {xuLyLichChieu(cumRap)}
+        </div>
+      );
+    });
+  };
   return (
     <div className="detailContentScheduleFilm">
       <div className="detailSchedule">
         <ul className="nav nav-tabs" id="tabListOfDay" role="tablist">
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link active"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
-          <li className="detail__listOfDay--item nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              data-toggle="tab"
-              role="tab"
-              aria-selected="true"
-            >
-              <p className="dayOfWeek">Thứ 3</p>
-              <p className="date">01</p>
-            </a>
-          </li>
+          {renderListOfDays()}
         </ul>
+
         <div className="nav detail__theater" role="tablist">
-          <div
-            className="logo__wrapper active"
-            data-toggle="tab"
-            role="tab"
-            data-target="#BHDStar"
-            aria-selected="true"
-          >
-            <div className="logo__detail">
-              <img
-                className="theaterList__image"
-                src="https://movie0706.cybersoft.edu.vn/hinhanh/bhd-star-cineplex.png"
-                alt="BHD Star Cineplex"
-              />
-              <span className="tenHeThong">BHD Star Cineplex</span>
-              <span className="arrow" />
-            </div>
-          </div>
-          <div
-            className="logo__wrapper"
-            data-toggle="tab"
-            role="tab"
-            data-target="#CGV"
-            aria-selected="false"
-          >
-            <div className="logo__detail">
-              <img
-                className="theaterList__image"
-                src="https://movie0706.cybersoft.edu.vn/hinhanh/cgv.png"
-                alt="cgv"
-              />
-              <span className="tenHeThong">cgv</span>
-              <span className="arrow" />
-            </div>
-          </div>
-          <div
-            className="logo__wrapper"
-            data-toggle="tab"
-            role="tab"
-            data-target="#CineStar"
-            aria-selected="false"
-          >
-            <div className="logo__detail">
-              <img
-                className="theaterList__image"
-                src="https://movie0706.cybersoft.edu.vn/hinhanh/cinestar.png"
-                alt="CineStar"
-              />
-              <span className="tenHeThong">CineStar</span>
-              <span className="arrow" />
-            </div>
-          </div>
-          <div
-            className="logo__wrapper"
-            data-toggle="tab"
-            role="tab"
-            data-target="#Galaxy"
-            aria-selected="false"
-          >
-            <div className="logo__detail">
-              <img
-                className="theaterList__image"
-                src="https://movie0706.cybersoft.edu.vn/hinhanh/galaxy-cinema.png"
-                alt="Galaxy Cinema"
-              />
-              <span className="tenHeThong">Galaxy Cinema</span>
-              <span className="arrow" />
-            </div>
-          </div>
-          <div
-            className="logo__wrapper"
-            data-toggle="tab"
-            role="tab"
-            data-target="#LotteCinima"
-            aria-selected="false"
-          >
-            <div className="logo__detail">
-              <img
-                className="theaterList__image"
-                src="https://movie0706.cybersoft.edu.vn/hinhanh/lotte-cinema.png"
-                alt="Lotte Cinema"
-              />
-              <span className="tenHeThong">Lotte Cinema</span>
-              <span className="arrow" />
-            </div>
-          </div>
-          <div
-            className="logo__wrapper"
-            data-toggle="tab"
-            role="tab"
-            data-target="#MegaGS"
-            aria-selected="false"
-          >
-            <div className="logo__detail">
-              <img
-                className="theaterList__image"
-                src="https://movie0706.cybersoft.edu.vn/hinhanh/megags.png"
-                alt="MegaGS"
-              />
-              <span className="tenHeThong">MegaGS</span>
-              <span className="arrow" />
-            </div>
-          </div>
+          {renderHeThongRap()}
         </div>
 
         <div className="detail__showList tab-content">
-          <div className="tab-pane fade active show" id="BHDStar">
-            <div className="alert alert-info">
-              Hiện không có lịch chiếu trên hệ thống rạp này
-            </div>
-          </div>
-          <div className="tab-pane fade" id="CGV">
-            <div className="wrapper__collapse ">
-              <div
-                className="main__collapse"
-                data-toggle="collapse"
-                data-target="#CGV_cgv-crescent-mall"
-              >
-                <div className="cinema__details--item">
-                  <img
-                    className="theaterList__image"
-                    src="/images/bhd-star-bitexco-15379552241200.jpg"
-                    alt="cgv"
-                  />
-                  <div className="wrapInfo">
-                    <span className="chiNhanh">
-                      <span className="tenRap CGV">CGV</span> - Crescent Mall
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="collapse" id="CGV_cgv-crescent-mall">
-                <div className="pt-3 row content__collapse stack">
-                  <div className="col-12" style={{ padding: "0 15px" }}>
-                    2D Digital
-                  </div>
-                  <div className="col-12">
-                    <button className="btn btn-time">
-                      <span className="start">10:10</span> ~ 12:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">12:10</span> ~ 14:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">14:10</span> ~ 16:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">16:10</span> ~ 18:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">18:10</span> ~ 20:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">20:10</span> ~ 22:10
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="tab-pane fade" id="CineStar">
-            <div className="alert alert-info">
-              Hiện không có lịch chiếu trên hệ thống rạp này
-            </div>
-          </div>
-          <div className="tab-pane fade" id="Galaxy">
-            <div className="wrapper__collapse ">
-              <div
-                className="main__collapse"
-                data-toggle="collapse"
-                data-target="#Galaxy_glx-tan-binh"
-              >
-                <div className="cinema__details--item">
-                  <img
-                    className="theaterList__image"
-                    src="/images/Galaxy_theater.jpg"
-                    alt="Galaxy Cinema"
-                  />
-                  <div className="wrapInfo">
-                    <span className="chiNhanh">
-                      <span className="tenRap GLX">GLX</span> - Tân Bình
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="collapse" id="Galaxy_glx-tan-binh">
-                <div className="pt-3 row content__collapse stack">
-                  <div className="col-12">2D Digital</div>
-                  <div className="col-12">
-                    <button className="btn btn-time">
-                      <span className="start">10:10</span> ~ 12:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">12:10</span> ~ 14:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">14:10</span> ~ 16:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">16:10</span> ~ 18:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">18:10</span> ~ 20:10
-                    </button>
-                    <button className="btn btn-time">
-                      <span className="start">20:10</span> ~ 22:10
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="tab-pane fade" id="LotteCinima">
-            <div className="alert alert-info">
-              Hiện không có lịch chiếu trên hệ thống rạp này
-            </div>
-          </div>
-          <div className="tab-pane fade" id="MegaGS">
-            <div className="alert alert-info">
-              Hiện không có lịch chiếu trên hệ thống rạp này
-            </div>
-          </div>
+          {renderLichChieuChiNhanh()}
         </div>
       </div>
     </div>
--- src/containers/HomeTemPlate/FilmDetail/index.jsx
+++ src/containers/HomeTemPlate/FilmDetail/index.jsx
@@ -1,22 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
 import { useLocation, useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 import FilmInfoBottom from "../../../components/FilmInfoBottom";
 import FilmInfoTop from "../../../components/FilmInfoTop";
 import Footer from "../../../components/Footer";
 import NavbarHome from "../../../components/NavbarHome";
 import ScrollTopArrow from "../../../components/ScrollTopArrow";
+import { fetchShowtimeFilm } from "../../AdminTemplate/ShowTimesManagement/modules/action";
 import "./style.css";
 export default function FilmDetail() {
   // const { film, filmList, idx } = useLocation().state;
+  const dispatch = useDispatch();
   const dataPass = useLocation().state;
   const { film, filmList, idx } = dataPass;
-
+  useEffect(() => {
+    dispatch(fetchShowtimeFilm(film.maPhim));
+  }, []);
+  const showTimesFilm = useSelector((state) => state.fetchShowtimeReducer);
   return (
     <div className="film-detail">
       <NavbarHome />
       <div className="filmDetailContent">
         <FilmInfoTop film={film} filmList={filmList} idx={idx} />
-        <FilmInfoBottom film={film} />
+        <FilmInfoBottom film={film} showTimesFilm={showTimesFilm.data} />
       </div>
       <Footer />
       <ScrollTopArrow />
